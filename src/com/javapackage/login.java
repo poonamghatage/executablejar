01fe8db53feac6e78a89103a9d0effe36e270600
Trunal <trunal.thakre@perennialsys.com>
2020-12-23 14:58:02 +0530

    first commit

---
+++ src/com/javapackage/login.java
@@ -0,0 +1,12 @@
+package com.javapackage;
+
+import java.io.IOException;
+
+
+public class login {
+    public static void main(String arg[]) throws IOException {
+
+        definition.openbrowser();
+    }
+}
+
